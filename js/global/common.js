32b0a101a16d07875b22a277764929d76c1491b2
Mindia Arabuli <65043119+Mind1a@users.noreply.github.com>
2025-03-04 22:07:29 +0400

    update

---
+++ js/global/common.js
@@ -0,0 +1,15 @@
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("showMenu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("showMenu");
+  });
+}
